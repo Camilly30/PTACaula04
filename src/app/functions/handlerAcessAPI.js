4cd4b3695f6d558f20a815f6fd0fc6032ac7d998
Camilly30 <millly.bacarim@gmail.com>
2023-08-15 17:38:34 -0400

    commit 01

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,10 +1,36 @@
 'use server'
 
 const getUserAuthenticated = (user) => {
-
+  const user = [
+        {
+        name:'Marcelino',
+        email:'marcelino@teste.com',
+        password:'123456',
+        token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+       },
+       {
+        name:'Ahsoka Tano',
+        email:'padawan@legalbrow.com',
+        password:'6666666',
+        token:''
+    },
+       {
+        name:'Anakin Skywalker',
+        email:'nowimthemaster@legalbrow.com',
+        password:'555555',
+        token:''
+       },
+       {
+        name:'Padmé',
+        email:'legacy@legalbrow.com',
+        password:'555555',
+        token:''
+       }
+];
 }
 
 const getUsers = () =>{
+   
         
 }
 export { getUsers, getUserAuthenticated };
